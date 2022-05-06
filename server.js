d54cc1ef0b5fb84f0b5baf4fd80db7836225daf7
JoSeongYoon <whtjddbs0112@naver.com>
2022-05-06 15:42:08 +0900

    aws,database connect

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,26 @@ const port = process.env.port || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://placeimg.com/48/48/1',
-            'name': '조성윤',
-            'birthday': '990112',
-            'gender': '남자',
-            'job': '대학생'
-        },
-        {
-            'id': 2,
-            'image': 'https://placeimg.com/48/48/2',
-            'name': '생활코딩',
-            'birthday': '999999',
-            'gender': '남자',
-            'job': '프로그래머'
-        },
-        {
-            'id': 3,
-            'image': 'https://placeimg.com/48/48/3',
-            'name': '나동빈',
-            'birthday': '961127',
-            'gender': '남자',
-            'job': '공대생'
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err,rows,fields) =>{
+            res.send(rows);
         }
-    ]);
+    );
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
